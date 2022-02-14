fd0b13924f312ed7d1524fa983985809d119bb58
Daniel Santiago <danielsantiago@outlook.com>
2022-02-14 17:35:45 +0200

    scaffold results feature module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'results',
+    loadChildren: () =>
+      import('./results/results.module').then((m) => m.ResultsModule),
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
