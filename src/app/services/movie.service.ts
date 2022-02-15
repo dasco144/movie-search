41b00846e2bbdddf6c1c1b7d382c037ff78ab373
Daniel Santiago <danielsantiago@outlook.com>
2022-02-15 19:19:26 +0200

    restrict search to only return movies

--- src/app/services/movie.service.ts
+++ src/app/services/movie.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpParams } from '@angular/common/http';
-import { map, Observable } from 'rxjs';
+import { BehaviorSubject, Observable } from 'rxjs';
 import { environment } from '../../environments/environment';
 import { MovieSearchResult } from '../models/movie-search-result';
 import { MovieDetail } from '../models/movie-detail';
@@ -17,6 +17,14 @@ type MovieSearchResponse = {
 export class MovieService {
   private readonly apiUrl = environment.apiUrl;
 
+  private userFavourites: string[] = [];
+
+  private userFavouritesSubj = new BehaviorSubject<string[]>(
+    this.userFavourites
+  );
+
+  userFavourites$ = this.userFavouritesSubj.asObservable();
+
   constructor(private httpClient: HttpClient) {}
 
   search(title: string): Observable<{
@@ -24,7 +32,9 @@ export class MovieService {
     Search: MovieSearchResult[];
     totalResults: number;
   }> {
-    const params: HttpParams = new HttpParams().set('s', title);
+    const params: HttpParams = new HttpParams()
+      .set('s', title)
+      .set('type', 'movie');
 
     return this.httpClient.get<MovieSearchResponse>(this.apiUrl, { params });
   }
@@ -33,5 +43,32 @@ export class MovieService {
     const params: HttpParams = new HttpParams().set('i', id);
 
     return this.httpClient.get<MovieDetail>(this.apiUrl, { params });
+  }
+
+  toggleFavourite(id: string): void {
+    if (this.userFavourites.includes(id)) {
+      this.removeFromFavourites(id);
+      return;
+    }
+
+    this.addToFavourites(id);
+  }
+
+  addToFavourites(id: string): void {
+    if (this.userFavourites.includes(id)) {
+      return;
+    }
+
+    this.userFavourites.push(id);
+    this.userFavouritesSubj.next(this.userFavourites);
+  }
+
+  removeFromFavourites(id: string): void {
+    if (!this.userFavourites.includes(id)) {
+      return;
+    }
+
+    this.userFavourites = this.userFavourites.filter((favId) => id !== favId);
+    this.userFavouritesSubj.next(this.userFavourites);
   }
 }
