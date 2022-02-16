32783f2c16ee7e439877379e247310a6ee4ad90e
Daniel Santiago <danielsantiago@outlook.com>
2022-02-16 08:53:35 +0200

    * add paging support for movies component * handle no movies found

--- src/app/components/movies/movies.component.ts
+++ src/app/components/movies/movies.component.ts
@@ -13,6 +13,11 @@ import { MovieService } from '../../services/movie.service';
 export class MoviesComponent implements OnInit {
   movies$!: Observable<MovieSearchResult[]>;
 
+  moviesCount = 0;
+
+  // api page size is set to 10
+  pageSize = 10;
+
   constructor(
     private movieService: MovieService,
     private snackBar: MatSnackBar,
@@ -21,30 +26,40 @@ export class MoviesComponent implements OnInit {
   ) {}
 
   ngOnInit(): void {
+    // Load the first page on initialize
+    this.search();
+  }
+
+  search(pageNo?: number): void {
+    // Get the query params from the activatedRoute
     this.movies$ = this.activatedRoute.queryParams.pipe(
       map((params) => {
+        // If no search query parameter or falsy value provided, then throw error to be handled by catch error below
         if (!params['search']) {
           throw new Error('Invalid search term');
         }
         return params['search'];
       }),
-      switchMap((search) => this.movieService.search(search)),
+      // Search for movies using the query param search
+      switchMap((search) => this.movieService.search(search, pageNo)),
       map((result) => {
+        // If we get no results then display a toast and return an empty array
         if (!result.Search?.length) {
-          this.snackBar.open('No results found', undefined, {
-            duration: 5000,
-            verticalPosition: 'top',
-          });
           return [];
         }
+
+        // Set the count total count for the paging
+        this.moviesCount = result.totalResults;
 
         return result.Search;
       }),
       catchError((error: Error) => {
+        // Catch any errors and display the error message in a snack bar
         this.snackBar.open(error.message, undefined, {
           duration: 5000,
           verticalPosition: 'top',
         });
+        // Navigate back to the search page
         void this.router.navigate(['../'], { relativeTo: this.activatedRoute });
         return [];
       })
