c66f0c8b0da6bc7817a0ff19d7293a6492be4096
Daniel Santiago <danielsantiago@outlook.com>
2022-02-16 08:32:02 +0200

    handle no favourites being set

--- src/app/components/favourites/favourites.component.ts
+++ src/app/components/favourites/favourites.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { ActivatedRoute, Router } from '@angular/router';
-import { catchError, forkJoin, Observable, switchMap } from 'rxjs';
+import { catchError, forkJoin, map, Observable, of, switchMap } from 'rxjs';
 import { MovieDetail } from '../../models/movie-detail';
 import { MovieService } from '../../services/movie.service';
 
@@ -23,8 +23,15 @@ export class FavouritesComponent implements OnInit {
   ngOnInit(): void {
     this.favouriteMovies$ = this.movieService.userFavourites$.pipe(
       switchMap((favourites) => {
+        // If we have no favourites just return an empty array
+        if (!favourites.length) {
+          return of([]);
+        }
+
         // Since the api doesn't have the ability to get multiple movies by their imdb ids,
         // I will instead request each movie individually and wait for their observables to complete via the forkJoin function
+        // Ideally, the backend would have an api to request multiple movies via their imdb ids in a single request
+
         const favouriteObs = favourites.map((favourite) =>
           this.movieService.getByImdbID(favourite)
         );
