4aa06686d7fd50bc0fe3d574b27fad5da3283ab2
Daniel Santiago <danielsantiago@outlook.com>
2022-02-17 21:54:22 +0200

    add common component tests

--- src/app/components/common/movies-grid/movies-grid.component.spec.ts
+++ src/app/components/common/movies-grid/movies-grid.component.spec.ts
@@ -1,4 +1,22 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import {
+  ComponentFixture,
+  fakeAsync,
+  TestBed,
+  tick,
+} from '@angular/core/testing';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatIcon, MatIconModule } from '@angular/material/icon';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { By } from '@angular/platform-browser';
+import { Router } from '@angular/router';
+import { RouterTestingModule } from '@angular/router/testing';
+import { MockModule, MockPipe, MockService } from 'ng-mocks';
+import { Observable, Subject } from 'rxjs';
+import { MovieDetail } from '../../../models/movie-detail';
+import { MoviePosterPipe } from '../../../pipes/movie-poster.pipe';
+import { MovieService } from '../../../services/movie.service';
+import { getNewMovieDetail } from '../../../testing/helpers.spec';
 
 import { MoviesGridComponent } from './movies-grid.component';
 
@@ -6,20 +24,103 @@ describe('MoviesGridComponent', () => {
   let component: MoviesGridComponent;
   let fixture: ComponentFixture<MoviesGridComponent>;
 
+  let movieService: MovieService;
+  let router: Router;
+  let movieDetails: MovieDetail[];
+
+  let userFavouritesSubj: Subject<string[]>;
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ MoviesGridComponent ]
-    })
-    .compileComponents();
+      declarations: [MoviesGridComponent, MockPipe(MoviePosterPipe)],
+      imports: [
+        RouterTestingModule,
+        MockModule(MatIconModule),
+        MockModule(MatTooltipModule),
+        MockModule(MatCardModule),
+        MockModule(MatButtonModule),
+      ],
+      providers: [
+        { provide: MovieService, useValue: MockService(MovieService) },
+      ],
+    }).compileComponents();
   });
 
   beforeEach(() => {
     fixture = TestBed.createComponent(MoviesGridComponent);
     component = fixture.componentInstance;
+
+    router = TestBed.inject(Router);
+    spyOn(router, 'navigate');
+
+    movieService = TestBed.inject(MovieService);
+    spyOn(movieService, 'toggleFavourite');
+
+    movieDetails = [];
+
+    for (let i = 1; i < 10; i++) {
+      movieDetails.push(getNewMovieDetail(i));
+    }
+
+    userFavouritesSubj = new Subject<string[]>();
+
+    movieService.userFavourites$ = userFavouritesSubj;
+
+    component.movies = movieDetails;
+
     fixture.detectChanges();
+
+    userFavouritesSubj.next([]);
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  it('should setup favourites observable', fakeAsync(() => {
+    expect(component.favourites$).toEqual(jasmine.any(Observable));
+
+    const favMovieIds = movieDetails
+      .slice(0, 3)
+      .map((favMovie) => favMovie.imdbID);
+
+    let favourites: string[] = [];
+    component.favourites$.subscribe((res) => (favourites = res));
+
+    userFavouritesSubj.next(favMovieIds);
+
+    tick();
+
+    expect(favourites).toEqual(favMovieIds);
+  }));
+
+  it('should navigate to movie detail route on open icon click', () => {
+    const movie = fixture.debugElement.query(By.css('.movie'));
+    const navigateIcon = movie.queryAll(By.directive(MatIcon))[1];
+
+    navigateIcon.triggerEventHandler('click', null);
+
+    expect(router.navigate).toHaveBeenCalledWith([
+      '/movies',
+      movieDetails[0].imdbID,
+    ]);
   });
+
+  it('should toggle favourite on star icon click', fakeAsync(() => {
+    const movie = fixture.debugElement.query(By.css('.movie'));
+    let favouriteIcon = movie.queryAll(By.directive(MatIcon))[0];
+    let favouriteIconEl = favouriteIcon.nativeElement as HTMLElement;
+
+    expect(favouriteIconEl.textContent?.trim()).toEqual('star_outline');
+
+    favouriteIcon.triggerEventHandler('click', null);
+    userFavouritesSubj.next([movieDetails[0].imdbID]);
+
+    tick();
+    fixture.detectChanges();
+
+    favouriteIcon = movie.queryAll(By.directive(MatIcon))[0];
+    favouriteIconEl = favouriteIcon.nativeElement as HTMLElement;
+
+    expect(favouriteIconEl.textContent?.trim()).toEqual('star');
+    expect(movieService.toggleFavourite).toHaveBeenCalledWith(
+      movieDetails[0].imdbID
+    );
+  }));
 });
--- src/app/components/common/theme-selector/theme-selector.component.spec.ts
+++ src/app/components/common/theme-selector/theme-selector.component.spec.ts
@@ -1,4 +1,10 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatMenuItem, MatMenuModule } from '@angular/material/menu';
+import { By } from '@angular/platform-browser';
+import { MockModule, MockService } from 'ng-mocks';
+import { ThemeService } from '../../../services/theme.service';
 
 import { ThemeSelectorComponent } from './theme-selector.component';
 
@@ -6,20 +12,58 @@ describe('ThemeSelectorComponent', () => {
   let component: ThemeSelectorComponent;
   let fixture: ComponentFixture<ThemeSelectorComponent>;
 
+  let themeService: ThemeService;
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ ThemeSelectorComponent ]
-    })
-    .compileComponents();
+      declarations: [ThemeSelectorComponent],
+      imports: [
+        MockModule(MatIconModule),
+        MockModule(MatButtonModule),
+        MockModule(MatMenuModule),
+      ],
+      providers: [
+        { provide: ThemeService, useValue: MockService(ThemeService) },
+      ],
+    }).compileComponents();
   });
 
   beforeEach(() => {
     fixture = TestBed.createComponent(ThemeSelectorComponent);
+
+    themeService = TestBed.inject(ThemeService);
+    spyOn(themeService, 'setTheme');
+
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  it('should load default theme', () => {
+    expect(component.currentTheme).toEqual(component.themes[0]);
+    expect(themeService.setTheme).toHaveBeenCalledWith(component.themes[0]);
+  });
+
+  it('should set theme on theme button click', () => {
+    const themeButtons = fixture.debugElement.queryAll(
+      By.directive(MatMenuItem)
+    );
+
+    themeButtons[1].triggerEventHandler('click', null);
+    fixture.detectChanges();
+
+    expect(component.currentTheme).toEqual(component.themes[1]);
+    expect(themeService.setTheme).toHaveBeenCalledWith(component.themes[1]);
+
+    themeButtons[2].triggerEventHandler('click', null);
+    fixture.detectChanges();
+
+    expect(component.currentTheme).toEqual(component.themes[2]);
+    expect(themeService.setTheme).toHaveBeenCalledWith(component.themes[2]);
+
+    themeButtons[3].triggerEventHandler('click', null);
+    fixture.detectChanges();
+
+    expect(component.currentTheme).toEqual(component.themes[3]);
+    expect(themeService.setTheme).toHaveBeenCalledWith(component.themes[3]);
   });
 });
